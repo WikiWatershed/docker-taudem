3f06c6396d11651ea4b3d4f3ed5949e7deb5bd78
Terence Tuhinanshu <terence@tuhinanshu.com>
2024-07-11 12:02:49 -0400

    Run console non-interactively on CI

--- scripts/console.sh
+++ scripts/console.sh
@@ -4,4 +4,9 @@ set -ex
 
 ARGS=$*
 
-docker run --rm -ti docker-taudem $ARGS
+if [[ -z $CI ]]; then
+	docker run --rm -ti docker-taudem $ARGS
+else
+	# Run in non-interactive mode in CI
+	docker run --rm docker-taudem $ARGS
+fi
